ef4b47238cb20380d2a22b5e59d71da9a209e92a
Ping <ping15512@gmail.com>
2024-01-18 13:15:18 +0700

    fix:debug

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
+import React from "react";
 
 function App() {
   const [products, setProducts] = useState([]);
-
+  //async
   useEffect(() => {
-    const fetchData = () => {
+    const fetchData = async () => {
       try {
         const response = await axios.get("https://dummyjson.com/products");
-        setProducts(response.data.data);
+        console.log("Data from API:", response.data); // ดูข้อมูลที่ได้จาก API
+        setProducts(response.data.products);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -26,7 +28,7 @@ function App() {
             <h2>{product.title}</h2>
             <p>Description: {product.description}</p>
             <p>Price: ${product.price}</p>
-            <img src={product.picture} alt={product.title} />
+            <img src={product.thumbnail} alt={product.title} />
           </div>
         ))}
       </div>
